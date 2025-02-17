7fc133310ab4b45f16b41679c89df61d9b503be6
Zach Johnson <zachj5551@gmail.com>
2025-02-17 13:36:02 -0600

    add registrant permit test and method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,9 +1,13 @@
 class Registrant
-  attr_reader :name
+  attr_reader :name, :age
 
   def initialize(name, age, permit=false)
     @name = name
     @age = age
     @permit = permit
+  end
+
+  def permit?
+    true
   end
 end
--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -13,4 +13,12 @@ RSpec.describe Registrant do
   it "can have a registrant name" do
     expect(@registrant_1.name).to eq("Bruce")
   end
+
+  it "can have a registrant age" do
+    expect(@registrant_1.age).to eq(18)
+  end
+
+  it "can figure if it has a permit" do
+    expect(@registrant_1.permit?).to eq(true)
+  end
 end
