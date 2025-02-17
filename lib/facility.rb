068534b83063c46962764d0e328149081f71da2c
Zach Johnson <zachj5551@gmail.com>
2025-02-17 16:39:37 -0600

    add test and attr_reader method for registered_vehicles

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,22 +1,23 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(facility_details)
       @name = facility_details[:name]
       @address = facility_details[:address]
       @phone = facility_details[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
   end
 
-  def registered_vehicles
-    @registered_vehicles = []
-  end
-
   def collected_fees
     @collected_fees = 0
+  end
+
+  def register_vehicle(vehicle)
+    @registered_vehicles << vehicle
   end
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Facility do
     @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
     @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-    bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-    camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -39,6 +39,10 @@ RSpec.describe Facility do
 
     it 'can list collected fees' do
       expect(@facility_1.collected_fees).to eq(0)
+    end
+
+    it 'registers the cruz vehicle' do
+      expect(@facility_1.register_vehicle(@cruz)).to eq([@cruz])
     end
   end
 end
